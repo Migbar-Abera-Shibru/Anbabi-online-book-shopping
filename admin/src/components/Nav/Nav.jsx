bd4101b2ea25b68b7cf05cd0ae50fb9afb75c0de
Migbar Abera Shibru <110468066+Migbar-Abera-Shibru@users.noreply.github.com>
2022-08-11 20:53:18 +0300

    Add files via upload

---
+++ admin/src/components/Nav/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+   <div className="nav">
+       <div className="left">
+           <span> Anababi</span>
+           
+           
+       </div>
+       <div className="right">
+           <ul>
+               
+               {/* <li>contacts</li>
+               <li>About</li> */}
+           </ul>
+       </div>
+   </div>
+  )
+}
+
+export default Nav
